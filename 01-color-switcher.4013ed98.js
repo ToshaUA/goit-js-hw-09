304420c7f53c86eb2f202b9d4cc4761e3e956214
ToshaUA <91492704+ToshaUA@users.noreply.github.com>
2022-10-09 12:06:00 +0000

    Deploying to gh-pages from @ ToshaUA/goit-js-hw-09@1008a5b64b51d32a62acb73606c9d21bdaebef24 🚀

--- 01-color-switcher.4013ed98.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled","true"),e.removeAttribute("disabled"),timerId=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){e.setAttribute("disabled","true"),t.removeAttribute("disabled"),clearInterval(timerId)}));
-//# sourceMappingURL=01-color-switcher.4013ed98.js.map
