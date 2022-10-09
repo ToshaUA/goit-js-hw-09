76bfcd4f552fe62230094eb088c9ea3bda1f1286
Anton Taran <91492704+ToshaUA@users.noreply.github.com>
2022-10-09 14:36:01 +0300

    task 1, 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startRandomBodyBackgroundColor =
+  document.querySelector('button[data-start]');
+const stopRandomBodyBackgroundColor =
+  document.querySelector('button[data-stop]');
+startRandomBodyBackgroundColor.addEventListener('click', getStart);
+stopRandomBodyBackgroundColor.addEventListener('click', getStop);
 
+function getStart() {
+  startRandomBodyBackgroundColor.setAttribute('disabled', 'true');
+  stopRandomBodyBackgroundColor.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function getStop() {
+  stopRandomBodyBackgroundColor.setAttribute('disabled', 'true');
+  startRandomBodyBackgroundColor.removeAttribute('disabled');
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
